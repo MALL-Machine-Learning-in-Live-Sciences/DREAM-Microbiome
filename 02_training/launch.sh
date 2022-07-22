4d4936286cd8e756004ed70a514b23051bb97024
unknown <joselb1993@gmail.com>
2022-07-22 15:44:48 +0200

    update

--- 02_training/launch.sh
+++ 02_training/launch.sh
@@ -2,4 +2,4 @@
 
 module load cesga/2018 gcc/6.4.0 R/4.0.2
 
-Rscript ~/git/tcga_coad_surv/Sunetal/sh-generator.r
+Rscript /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/DREAM-Microbiome/02_training/sh-generator.r
