d15c9e1c1c09dcdff21657eeea3321ce4312b395
jlinaresb <joselb1993@gmail.com>
2022-09-09 14:09:10 +0200

    updat

--- 03_explore_results/code/06_predict_by_cohort.r
+++ 03_explore_results/code/06_predict_by_cohort.r
@@ -1,18 +1,28 @@
 setwd('~/git/DREAM-Microbiome/02_training/results/nineth-experiment/')
 
+require(mlr3)
+require(mlr3pipelines)
+require(mlr3misc)
+require(mlr3benchmark)
+require(mlr3tuning)
+require(mlr3extralearners)
+require(mlr3learners)
+require(mlr3measures)
+
 measures = list(msr("classif.acc", id = "Accuracy"),msr("classif.auc", id = "AUCROC"),
                 msr("classif.prauc", id = "PRAUC"),msr("classif.sensitivity", id = "Sensitivity"),
                 msr("classif.specificity", id = "Specificity"))
 
-require(mlr3)
-files = list.files(pattern = 'all_32')
+preterm = 'all_28'  #all_28 all_32
+
+files = list.files(pattern = preterm)
 files = files[-grep('df_iter', files)]
-files = files[-grep('reduced', files)]
+files = files[grep('reduced', files)]
 files = files[grep('rf', files)]
 
 
-i = 3
-files[i]
+i = 1
+(name.train = gsub('_rf', '', files[i]))
 bmr = readRDS(files[i])
 bmr$aggregate()
 rr = bmr$aggregate()[learner_id == "classif.randomForest.tuned", resample_result][[1]]
@@ -22,7 +32,7 @@ pred = rr$prediction()
 list(pred$confusion, 
      pred$score(measures = measures))
 
-costs = matrix(c(0, 1, 2, 0), 2)
+costs = matrix(c(0, 1, 4, 0), 2)
 (thold = costs[2,1] / (costs[2,1] + costs[1,2]))
 threshold = c(preterm = thold,                                                  
               term = 1 - thold)
@@ -36,28 +46,75 @@ list(pred2$confusion,
 preds = rr$predictions()
 for (i in seq_along(preds)) {
   p = preds[[i]]
-  print(list(p$confusion))
+  p$set_threshold(threshold = threshold)
+  print(list(p$score(measures = measures)))
 }
 
 
 
 # Prediction by cohort
 # ===
-iter = 3
-data = as.data.table(bmr)
-outer_learners = map(data$learner, "learner")
-model = outer_learners[[iter]]
-model
+data = readRDS(paste0('~/git/DREAM-Microbiome/02_training/toRun/basal/', preterm, '.rds'))
+signature = colnames(readRDS(paste0('~/git/DREAM-Microbiome/02_training/toRun/basal_jlb_v3/', name.train)))
+data$target = as.factor(data$target)
+data[,grep('NIH.', colnames(data))] = apply(data[,grep('NIH.', colnames(data))], 2, function(x) as.numeric(x))
+# cohorts = readRDS('~/git/DREAM-Microbiome/02_training/data/task_preterm_by_cohort.rds')
+str(data)
+cohortA = data[grep('A', rownames(data)), match(signature, colnames(data))]
+cohortC = data[grep('C', rownames(data)), match(signature, colnames(data))]
+cohortD = data[grep('D', rownames(data)), match(signature, colnames(data))]
+cohortE = data[grep('E', rownames(data)), match(signature, colnames(data))]
+cohortF = data[grep('F', rownames(data)), match(signature, colnames(data))]
+cohortG = data[grep('G', rownames(data)), match(signature, colnames(data))]
+cohortH = data[grep('H', rownames(data)), match(signature, colnames(data))]
+cohortI = data[grep('I', rownames(data)), match(signature, colnames(data))]
 
-cohorts = readRDS('~/git/DREAM-Microbiome/02_training/data/task_preterm_by_cohort.rds')
-test = cohorts$G
-extPred = model$predict(task = test)
-measures = list(msr("classif.acc", id = "Accuracy"),msr("classif.auc", id = "AUCROC"),
-                msr("classif.prauc", id = "PRAUC"),msr("classif.sensitivity", id = "Sensitivity"),
-                msr("classif.specificity", id = "Specificity"))
-extPred$set_threshold(threshold) 
-list(extPred$confusion, 
-     extPred$score(measures = measures))
+require(dplyr)
+cohortI$id = sapply(strsplit(rownames(cohortI), '-'), '[[', 1)
+I.target = cohortI %>%
+  select(id, target) %>% 
+  group_by(id) %>% 
+  slice(1)
+cohortI = cohortI %>% 
+  group_by(id) %>%
+  summarise(across(-target, median)) %>% 
+  mutate(target = I.target$target) %>% 
+  select(-id)
+rownames(cohortI) = I.target$id
+
+
+
+makeTask = function(name, data){
+  task = TaskClassif$new(id = name,
+                         backend = data,
+                         target = "target",
+                         positive ="preterm")
+  return(task)
+}
+
+cohortA = makeTask('cohortA', cohortA)
+cohortC = makeTask('cohortC', cohortC)
+cohortD = makeTask('cohortD', cohortD)
+cohortE = makeTask('cohortE', cohortE)
+cohortF = makeTask('cohortF', cohortF)
+cohortG = makeTask('cohortG', cohortG)
+cohortH = makeTask('cohortH', cohortH)
+cohortI = makeTask('cohortI', cohortI)
+
+# get model
+for (iter in 1:50) {
+  data = as.data.table(bmr)
+  outer_learners = map(data$learner, "learner")
+  model = outer_learners[[iter]]
+  model
+  
+  extPred = model$predict(task = cohortI)
+  extPred$set_threshold(threshold) 
+  print(list(
+    extPred$confusion,
+    extPred$score(measures = measures)))
+}
+
 
 
 
